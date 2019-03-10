e4738f18330d5a227b9e11cbfd626f3d5d7e6249
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-03-10 19:09:28 -0300

    Add local changelog
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/updater/UpdatesActivity.java
+++ src/com/pixeldust/updater/UpdatesActivity.java
@@ -167,9 +167,7 @@ public class UpdatesActivity extends UpdatesListActivity {
                 return true;
             }
             case R.id.menu_show_changelog: {
-                Intent openUrl = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse(Utils.getChangelogURL(this)));
-                startActivity(openUrl);
+                startActivity(new Intent(this, LocalChangelogActivity.class));
                 return true;
             }
         }
