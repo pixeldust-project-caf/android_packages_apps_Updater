PixelExperience/packages_apps_Updates

Cleanup stored zips if no updates available
Change-Id: I6cd66f70d2dd3d4219cca78c030c4ddad16f3948

src/org/pixelexperience/ota/UpdaterReceiver.java
@@ -29,6 +29,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v7.preference.PreferenceManager;
 
 import org.pixelexperience.ota.misc.Constants;
+import org.pixelexperience.ota.misc.Utils;
 
 public class UpdaterReceiver extends BroadcastReceiver {
 
@@ -91,6 +92,8 @@ public class UpdaterReceiver extends BroadcastReceiver {
                 pref.edit().putBoolean(Constants.PREF_INSTALL_NOTIFIED, true).apply();
                 showUpdateFailedNotification(context);
             }
+
+            Utils.cleanupDownloadsDir(context);
         }
     }
 }

pixeldust-project-caf/android_packages_apps_Updater

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/pixeldust/updater/UpdaterReceiver.java
@@ -28,6 +28,7 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v7.preference.PreferenceManager;
 
 import com.pixeldust.updater.misc.Constants;
+import com.pixeldust.updater.misc.Utils;
 
 public class UpdaterReceiver extends BroadcastReceiver {
 
@@ -90,6 +91,8 @@ public class UpdaterReceiver extends BroadcastReceiver {
                 pref.edit().putBoolean(Constants.PREF_INSTALL_NOTIFIED, true).apply();
                 showUpdateFailedNotification(context);
             }
+
+            Utils.cleanupDownloadsDir(context);
         }
     }
 }