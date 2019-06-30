fbf4767fea6154f093329cc7467aa062492710fc
spezi77 <spezi7713@gmx.net>
2019-06-30 22:59:41 +0200

    Utils: Fixed the logic to execute an OTA update. We should use PROP_VERSION_CODE = "ro.pixeldust.ota.version_code" to determine the ROM version flag.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/updater/misc/Utils.java
+++ src/com/pixeldust/updater/misc/Utils.java
@@ -102,7 +102,7 @@ public class Utils {
     }
 
     public static boolean isCompatible(UpdateBaseInfo update) {
-        if (update.getVersion().compareTo(SystemProperties.get(Constants.PROP_BUILD_VERSION)) < 0) {
+        if (update.getVersion().compareTo(SystemProperties.get(Constants.PROP_VERSION_CODE)) < 0) {
             Log.d(TAG, update.getName() + " is older than current Android version");
             return false;
         }
@@ -116,7 +116,7 @@ public class Utils {
     public static boolean canInstall(UpdateBaseInfo update) {
         return (update.getTimestamp() > SystemProperties.getLong(Constants.PROP_BUILD_DATE, 0)) &&
                 update.getVersion().equalsIgnoreCase(
-                        SystemProperties.get(Constants.PROP_BUILD_VERSION));
+                        SystemProperties.get(Constants.PROP_VERSION_CODE));
     }
 
     public static UpdateInfo parseJson(File file, boolean compatibleOnly)
