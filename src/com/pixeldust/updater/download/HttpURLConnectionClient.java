b54d21b7c9d0e510e070ecb0be10fb63241ebae5
spezi77 <spezi7713@gmx.net>
2019-07-01 19:21:20 +0200

    Update User-Agent for PD.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- src/com/pixeldust/updater/download/HttpURLConnectionClient.java
+++ src/com/pixeldust/updater/download/HttpURLConnectionClient.java
@@ -47,7 +47,7 @@ public class HttpURLConnectionClient implements DownloadClient {
                             DownloadClient.DownloadCallback callback,
                             boolean useDuplicateLinks) throws IOException {
         mClient = (HttpURLConnection) new URL(url).openConnection();
-        mClient.setRequestProperty("User-Agent", "org.pixelexperience.ota");
+        mClient.setRequestProperty("User-Agent", "com.pixeldust.updater");
         mDestination = destination;
         mProgressListener = progressListener;
         mCallback = callback;
@@ -166,7 +166,7 @@ public class HttpURLConnectionClient implements DownloadClient {
             String range = mClient.getRequestProperty("Range");
             mClient.disconnect();
             mClient = (HttpURLConnection) newUrl.openConnection();
-            mClient.setRequestProperty("User-Agent", "org.pixelexperience.ota");
+            mClient.setRequestProperty("User-Agent", "com.pixeldust.updater");
             if (range != null) {
                 mClient.setRequestProperty("Range", range);
             }
