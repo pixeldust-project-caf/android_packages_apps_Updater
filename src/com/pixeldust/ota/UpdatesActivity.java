PixelExperience/packages_apps_Updates

Fix force close when tapping on Install
Also, handle starting download case when on background

Change-Id: If218e7159535b0abaea2e4fad65a982bd8d760a6

src/org/pixelexperience/ota/UpdatesActivity.java
@@ -23,18 +23,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SimpleItemAnimator;
@@ -47,6 +52,8 @@ import org.pixelexperience.ota.controller.ABUpdateInstaller;
 import org.pixelexperience.ota.controller.UpdaterController;
 import org.pixelexperience.ota.controller.UpdaterService;
 import org.pixelexperience.ota.download.DownloadClient;
+import org.pixelexperience.ota.misc.Constants;
+import org.pixelexperience.ota.misc.PermissionsUtils;
 import org.pixelexperience.ota.misc.Utils;
 import org.pixelexperience.ota.model.UpdateInfo;
 import org.pixelexperience.ota.model.UpdateStatus;
@@ -60,6 +67,19 @@ import static org.pixelexperience.ota.model.UpdateStatus.UNKNOWN;
 public class UpdatesActivity extends UpdatesListActivity {
 
     private static final String TAG = "UpdatesActivity";
+
+    public static final String ACTION_START_DOWNLOAD_WITH_WARNING = "action_start_download_with_warning";
+
+    public static final String ACTION_SHOW_SNACKBAR = "action_show_snackbar";
+    public static final String EXTRA_SNACKBAR_TEXT = "extra_snackbar_text";
+
+    public static final String ACTION_EXPORT_UPDATE = "action_export_update";
+    public static final String EXTRA_UPDATE_NAME = "extra_snackbar_text";
+    public static final String EXTRA_UPDATE_FILE = "extra_update_file";
+
+    private String mExportUpdateName;
+    private File mExportUpdateFile;
+
     private UpdaterService mUpdaterService;
     private BroadcastReceiver mBroadcastReceiver;
 
@@ -90,7 +110,11 @@ public class UpdatesActivity extends UpdatesListActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
-        mAdapter.onRequestPermissionsResult(requestCode, grantResults);
+        if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                requestCode == ExportUpdateService.EXPORT_STATUS_PERMISSION_REQUEST_CODE) {
+            exportUpdate();
+        }
     }
 
     @Override
@@ -99,7 +123,7 @@ public class UpdatesActivity extends UpdatesListActivity {
         setContentView(R.layout.activity_updates);
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        mAdapter = new UpdatesListAdapter(this);
+        mAdapter = new UpdatesListAdapter();
         recyclerView.setAdapter(mAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -139,6 +163,14 @@ public class UpdatesActivity extends UpdatesListActivity {
                 } else if (ABUpdateInstaller.ACTION_RESTART_PENDING.equals(intent.getAction())) {
                     hideUpdates();
                     showRestartPendingDialog();
+                } else if (ACTION_START_DOWNLOAD_WITH_WARNING.equals(intent.getAction())) {
+                    startDownloadWithWarning();
+                } else if (ACTION_SHOW_SNACKBAR.equals(intent.getAction())) {
+                    showSnackbar(intent.getStringExtra(EXTRA_SNACKBAR_TEXT), Snackbar.LENGTH_LONG);
+                } else if (ACTION_EXPORT_UPDATE.equals(intent.getAction())) {
+                    mExportUpdateName = intent.getStringExtra(EXTRA_UPDATE_NAME);
+                    mExportUpdateFile = (File) intent.getSerializableExtra(EXTRA_UPDATE_FILE);
+                    exportUpdate();
                 }
             }
         };
@@ -217,6 +249,9 @@ public class UpdatesActivity extends UpdatesListActivity {
         intentFilter.addAction(UpdaterController.ACTION_UPDATE_CLEANUP_DONE);
         intentFilter.addAction(ExportUpdateService.ACTION_EXPORT_STATUS);
         intentFilter.addAction(ABUpdateInstaller.ACTION_RESTART_PENDING);
+        intentFilter.addAction(ACTION_START_DOWNLOAD_WITH_WARNING);
+        intentFilter.addAction(ACTION_SHOW_SNACKBAR);
+        intentFilter.addAction(ACTION_EXPORT_UPDATE);
         LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver, intentFilter);
     }
 
@@ -331,6 +366,9 @@ public class UpdatesActivity extends UpdatesListActivity {
                     if (!cancelled) {
                         showSnackbar(R.string.snack_updates_check_failed, Snackbar.LENGTH_LONG);
                     }
+                    new Handler().postDelayed(() -> {
+                        showUpdates();
+                    }, 1000);
                     refreshAnimationStop();
                 });
             }
@@ -444,8 +482,16 @@ public class UpdatesActivity extends UpdatesListActivity {
         snack.show();
     }
 
-    private void handleABInstallationFailed(){
-        if (Utils.getCurrentUpdateIsIncremental(this) && Utils.shouldUseIncremental(this)){
+    @Override
+    public void showSnackbar(String text, int duration) {
+        Snackbar snack = Snackbar.make(findViewById(R.id.view_snackbar), text, duration);
+        TextView tv = snack.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+        tv.setTextColor(getColor(R.color.text_primary));
+        snack.show();
+    }
+
+    private void handleABInstallationFailed() {
+        if (Utils.getCurrentUpdateIsIncremental(this) && Utils.shouldUseIncremental(this)) {
             new AlertDialog.Builder(this, R.style.AppTheme_AlertDialogStyle)
                     .setTitle(R.string.installing_update_error)
                     .setMessage(R.string.installing_update_ab_disable_incremental_summary)
@@ -456,12 +502,61 @@ public class UpdatesActivity extends UpdatesListActivity {
                             })
                     .setNegativeButton(android.R.string.cancel, null)
                     .show();
-        }else{
+        } else {
             showSnackbar(R.string.installing_update_error, Snackbar.LENGTH_LONG);
         }
     }
 
-    private void cleanupUpdates(){
+    private void startDownloadWithWarning() {
+        UpdaterController updaterController = mUpdaterService.getUpdaterController();
+        if (!Utils.isNetworkAvailable(this)) {
+            updaterController.notifyNetworkUnavailable();
+            return;
+        }
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean warn = preferences.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true);
+        if (Utils.isOnWifiOrEthernet(this) || !warn) {
+            updaterController.startDownload();
+            return;
+        }
+
+        View checkboxView = LayoutInflater.from(this).inflate(R.layout.checkbox_view, null);
+        CheckBox checkbox = checkboxView.findViewById(R.id.checkbox);
+        checkbox.setText(R.string.checkbox_mobile_data_warning);
+
+        new AlertDialog.Builder(this, R.style.AppTheme_AlertDialogStyle)
+                .setTitle(R.string.update_on_mobile_data_title)
+                .setMessage(R.string.update_on_mobile_data_message)
+                .setView(checkboxView)
+                .setPositiveButton(R.string.action_download,
+                        (dialog, which) -> {
+                            if (checkbox.isChecked()) {
+                                preferences.edit()
+                                        .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, false)
+                                        .apply();
+                                supportInvalidateOptionsMenu();
+                            }
+                            updaterController.startDownload();
+                        })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
+    }
+
+    private void exportUpdate() {
+        boolean hasPermission = PermissionsUtils.checkAndRequestStoragePermission(
+                this, ExportUpdateService.EXPORT_STATUS_PERMISSION_REQUEST_CODE);
+        if (!hasPermission) {
+            return;
+        }
+        File dest = new File(Utils.getExportPath(), mExportUpdateName);
+        Intent intent = new Intent(this, ExportUpdateService.class);
+        intent.setAction(ExportUpdateService.ACTION_START_EXPORTING);
+        intent.putExtra(ExportUpdateService.EXTRA_SOURCE_FILE, mExportUpdateFile);
+        intent.putExtra(ExportUpdateService.EXTRA_DEST_FILE, dest);
+        startService(intent);
+    }
+
+    private void cleanupUpdates() {
         mAdapter.setDownloadId(null);
         mAdapter.notifyDataSetChanged();
         downloadUpdatesList(false);

pixeldust-project-caf/android_packages_apps_Updater

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/pixeldust/ota/UpdatesActivity.java
@@ -23,19 +23,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SimpleItemAnimator;
@@ -47,6 +52,8 @@ import com.pixeldust.ota.controller.ABUpdateInstaller;
 import com.pixeldust.ota.controller.UpdaterController;
 import com.pixeldust.ota.controller.UpdaterService;
 import com.pixeldust.ota.download.DownloadClient;
+import com.pixeldust.ota.misc.Constants;
+import com.pixeldust.ota.misc.PermissionsUtils;
 import com.pixeldust.ota.misc.Utils;
 import com.pixeldust.ota.model.UpdateInfo;
 import com.pixeldust.ota.model.UpdateStatus;
@@ -62,6 +69,19 @@ import static com.pixeldust.ota.model.UpdateStatus.UNKNOWN;
 public class UpdatesActivity extends UpdatesListActivity {
 
     private static final String TAG = "UpdatesActivity";
+
+    public static final String ACTION_START_DOWNLOAD_WITH_WARNING = "action_start_download_with_warning";
+
+    public static final String ACTION_SHOW_SNACKBAR = "action_show_snackbar";
+    public static final String EXTRA_SNACKBAR_TEXT = "extra_snackbar_text";
+
+    public static final String ACTION_EXPORT_UPDATE = "action_export_update";
+    public static final String EXTRA_UPDATE_NAME = "extra_snackbar_text";
+    public static final String EXTRA_UPDATE_FILE = "extra_update_file";
+
+    private String mExportUpdateName;
+    private File mExportUpdateFile;
+
     private UpdaterService mUpdaterService;
     private BroadcastReceiver mBroadcastReceiver;
 
@@ -92,7 +112,11 @@ public class UpdatesActivity extends UpdatesListActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String[] permissions, int[] grantResults) {
-        mAdapter.onRequestPermissionsResult(requestCode, grantResults);
+        if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                requestCode == ExportUpdateService.EXPORT_STATUS_PERMISSION_REQUEST_CODE) {
+            exportUpdate();
+        }
     }
 
     @Override
@@ -105,7 +129,7 @@ public class UpdatesActivity extends UpdatesListActivity {
                 .getString(R.string.security_patch_level), Utils.getSecurityPatchLevel()));
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        mAdapter = new UpdatesListAdapter(this);
+        mAdapter = new UpdatesListAdapter();
         recyclerView.setAdapter(mAdapter);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -145,6 +169,14 @@ public class UpdatesActivity extends UpdatesListActivity {
                 } else if (ABUpdateInstaller.ACTION_RESTART_PENDING.equals(intent.getAction())) {
                     hideUpdates();
                     showRestartPendingDialog();
+                } else if (ACTION_START_DOWNLOAD_WITH_WARNING.equals(intent.getAction())) {
+                    startDownloadWithWarning();
+                } else if (ACTION_SHOW_SNACKBAR.equals(intent.getAction())) {
+                    showSnackbar(intent.getStringExtra(EXTRA_SNACKBAR_TEXT), Snackbar.LENGTH_LONG);
+                } else if (ACTION_EXPORT_UPDATE.equals(intent.getAction())) {
+                    mExportUpdateName = intent.getStringExtra(EXTRA_UPDATE_NAME);
+                    mExportUpdateFile = (File) intent.getSerializableExtra(EXTRA_UPDATE_FILE);
+                    exportUpdate();
                 }
             }
         };
@@ -223,6 +255,9 @@ public class UpdatesActivity extends UpdatesListActivity {
         intentFilter.addAction(UpdaterController.ACTION_UPDATE_CLEANUP_DONE);
         intentFilter.addAction(ExportUpdateService.ACTION_EXPORT_STATUS);
         intentFilter.addAction(ABUpdateInstaller.ACTION_RESTART_PENDING);
+        intentFilter.addAction(ACTION_START_DOWNLOAD_WITH_WARNING);
+        intentFilter.addAction(ACTION_SHOW_SNACKBAR);
+        intentFilter.addAction(ACTION_EXPORT_UPDATE);
         LocalBroadcastManager.getInstance(this).registerReceiver(mBroadcastReceiver, intentFilter);
     }
 
@@ -337,6 +372,9 @@ public class UpdatesActivity extends UpdatesListActivity {
                     if (!cancelled) {
                         showSnackbar(R.string.snack_updates_check_failed, Snackbar.LENGTH_LONG);
                     }
+                    new Handler().postDelayed(() -> {
+                        showUpdates();
+                    }, 1000);
                     refreshAnimationStop();
                 });
             }
@@ -450,8 +488,16 @@ public class UpdatesActivity extends UpdatesListActivity {
         snack.show();
     }
 
-    private void handleABInstallationFailed(){
-        if (Utils.getCurrentUpdateIsIncremental(this) && Utils.shouldUseIncremental(this)){
+    @Override
+    public void showSnackbar(String text, int duration) {
+        Snackbar snack = Snackbar.make(findViewById(R.id.view_snackbar), text, duration);
+        TextView tv = snack.getView().findViewById(com.google.android.material.R.id.snackbar_text);
+        tv.setTextColor(getColor(R.color.text_primary));
+        snack.show();
+    }
+
+    private void handleABInstallationFailed() {
+        if (Utils.getCurrentUpdateIsIncremental(this) && Utils.shouldUseIncremental(this)) {
             new AlertDialog.Builder(this, R.style.AppTheme_AlertDialogStyle)
                     .setTitle(R.string.installing_update_error)
                     .setMessage(R.string.installing_update_ab_disable_incremental_summary)
@@ -462,12 +508,61 @@ public class UpdatesActivity extends UpdatesListActivity {
                             })
                     .setNegativeButton(android.R.string.cancel, null)
                     .show();
-        }else{
+        } else {
             showSnackbar(R.string.installing_update_error, Snackbar.LENGTH_LONG);
         }
     }
 
-    private void cleanupUpdates(){
+    private void startDownloadWithWarning() {
+        UpdaterController updaterController = mUpdaterService.getUpdaterController();
+        if (!Utils.isNetworkAvailable(this)) {
+            updaterController.notifyNetworkUnavailable();
+            return;
+        }
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean warn = preferences.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true);
+        if (Utils.isOnWifiOrEthernet(this) || !warn) {
+            updaterController.startDownload();
+            return;
+        }
+
+        View checkboxView = LayoutInflater.from(this).inflate(R.layout.checkbox_view, null);
+        CheckBox checkbox = checkboxView.findViewById(R.id.checkbox);
+        checkbox.setText(R.string.checkbox_mobile_data_warning);
+
+        new AlertDialog.Builder(this, R.style.AppTheme_AlertDialogStyle)
+                .setTitle(R.string.update_on_mobile_data_title)
+                .setMessage(R.string.update_on_mobile_data_message)
+                .setView(checkboxView)
+                .setPositiveButton(R.string.action_download,
+                        (dialog, which) -> {
+                            if (checkbox.isChecked()) {
+                                preferences.edit()
+                                        .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, false)
+                                        .apply();
+                                supportInvalidateOptionsMenu();
+                            }
+                            updaterController.startDownload();
+                        })
+                .setNegativeButton(android.R.string.cancel, null)
+                .show();
+    }
+
+    private void exportUpdate() {
+        boolean hasPermission = PermissionsUtils.checkAndRequestStoragePermission(
+                this, ExportUpdateService.EXPORT_STATUS_PERMISSION_REQUEST_CODE);
+        if (!hasPermission) {
+            return;
+        }
+        File dest = new File(Utils.getExportPath(), mExportUpdateName);
+        Intent intent = new Intent(this, ExportUpdateService.class);
+        intent.setAction(ExportUpdateService.ACTION_START_EXPORTING);
+        intent.putExtra(ExportUpdateService.EXTRA_SOURCE_FILE, mExportUpdateFile);
+        intent.putExtra(ExportUpdateService.EXTRA_DEST_FILE, dest);
+        startService(intent);
+    }
+
+    private void cleanupUpdates() {
         mAdapter.setDownloadId(null);
         mAdapter.notifyDataSetChanged();
         downloadUpdatesList(false);