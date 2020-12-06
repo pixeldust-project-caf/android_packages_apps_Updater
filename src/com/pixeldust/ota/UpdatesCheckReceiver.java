PixelExperience/packages_apps_Updates

Don't notify about new update if already downloading/installing
Change-Id: Iac77369cc79d1630a5fc68ec0e9abd1ba56cc875

src/org/pixelexperience/ota/UpdatesCheckReceiver.java
@@ -29,8 +29,10 @@ import android.util.Log;
 import androidx.core.app.NotificationCompat;
 
 import org.json.JSONException;
+import org.pixelexperience.ota.controller.ABUpdateInstaller;
 import org.pixelexperience.ota.download.DownloadClient;
 import org.pixelexperience.ota.misc.Utils;
+import org.pixelexperience.ota.model.UpdateStatus;
 
 import java.io.File;
 import java.io.IOException;
@@ -46,8 +48,12 @@ public class UpdatesCheckReceiver extends BroadcastReceiver {
 
     private static final String NEW_UPDATES_NOTIFICATION_CHANNEL =
             "new_updates_notification_channel";
+    public static final int NOTIFICATION_ID = 20;
 
     private static void showNotification(Context context) {
+        if (ABUpdateInstaller.needsReboot() || Utils.getPersistentStatus(context) != UpdateStatus.Persistent.UNKNOWN) {
+            return;
+        }
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationChannel notificationChannel = new NotificationChannel(
@@ -64,7 +70,7 @@ public class UpdatesCheckReceiver extends BroadcastReceiver {
         notificationBuilder.setContentTitle(context.getString(R.string.update_found_notification));
         notificationBuilder.setAutoCancel(true);
         notificationManager.createNotificationChannel(notificationChannel);
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 
     private static PendingIntent getRepeatingUpdatesCheckIntent(Context context) {

src/org/pixelexperience/ota/controller/UpdaterService.java
@@ -34,6 +34,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import org.pixelexperience.ota.R;
 import org.pixelexperience.ota.UpdaterReceiver;
 import org.pixelexperience.ota.UpdatesActivity;
+import org.pixelexperience.ota.UpdatesCheckReceiver;
 import org.pixelexperience.ota.misc.Utils;
 import org.pixelexperience.ota.model.Update;
 import org.pixelexperience.ota.model.UpdateInfo;
@@ -208,7 +209,8 @@ public class UpdaterService extends Service {
     }
 
     private void handleUpdateStatusChange(UpdateStatus status) {
-        if (ABUpdateInstaller.needsReboot()){
+        mNotificationManager.cancel(UpdatesCheckReceiver.NOTIFICATION_ID);
+        if (ABUpdateInstaller.needsReboot()) {
             return;
         }
         Update update = mUpdaterController.getCurrentUpdate();
@@ -221,6 +223,7 @@ public class UpdaterService extends Service {
                 break;
             }
             case STARTING: {
+                mNotificationManager.cancel(NOTIFICATION_ID);
                 mNotificationBuilder.mActions.clear();
                 mNotificationBuilder.setProgress(0, 0, true);
                 String text = getString(R.string.download_starting_notification);

pixeldust-project-caf/android_packages_apps_Updater

Signed-off-by: spezi77 <spezi7713@gmx.net>

src/com/pixeldust/ota/UpdatesCheckReceiver.java
@@ -28,14 +28,17 @@ import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 
-import org.json.JSONException;
+import com.pixeldust.ota.controller.ABUpdateInstaller;
 import com.pixeldust.ota.download.DownloadClient;
 import com.pixeldust.ota.misc.Utils;
+import com.pixeldust.ota.model.UpdateStatus;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 import java.util.UUID;
+
+import org.json.JSONException;
 
 public class UpdatesCheckReceiver extends BroadcastReceiver {
 
@@ -46,8 +49,12 @@ public class UpdatesCheckReceiver extends BroadcastReceiver {
 
     private static final String NEW_UPDATES_NOTIFICATION_CHANNEL =
             "new_updates_notification_channel";
+    public static final int NOTIFICATION_ID = 20;
 
     private static void showNotification(Context context) {
+        if (ABUpdateInstaller.needsReboot() || Utils.getPersistentStatus(context) != UpdateStatus.Persistent.UNKNOWN) {
+            return;
+        }
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         NotificationChannel notificationChannel = new NotificationChannel(
@@ -64,7 +71,7 @@ public class UpdatesCheckReceiver extends BroadcastReceiver {
         notificationBuilder.setContentTitle(context.getString(R.string.update_found_notification));
         notificationBuilder.setAutoCancel(true);
         notificationManager.createNotificationChannel(notificationChannel);
-        notificationManager.notify(0, notificationBuilder.build());
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
     }
 
     private static PendingIntent getRepeatingUpdatesCheckIntent(Context context) {

src/com/pixeldust/ota/controller/UpdaterService.java
@@ -34,6 +34,7 @@ import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import com.pixeldust.ota.R;
 import com.pixeldust.ota.UpdaterReceiver;
 import com.pixeldust.ota.UpdatesActivity;
+import com.pixeldust.ota.UpdatesCheckReceiver;
 import com.pixeldust.ota.misc.Utils;
 import com.pixeldust.ota.model.Update;
 import com.pixeldust.ota.model.UpdateInfo;
@@ -208,7 +209,8 @@ public class UpdaterService extends Service {
     }
 
     private void handleUpdateStatusChange(UpdateStatus status) {
-        if (ABUpdateInstaller.needsReboot()){
+        mNotificationManager.cancel(UpdatesCheckReceiver.NOTIFICATION_ID);
+        if (ABUpdateInstaller.needsReboot()) {
             return;
         }
         Update update = mUpdaterController.getCurrentUpdate();
@@ -221,6 +223,7 @@ public class UpdaterService extends Service {
                 break;
             }
             case STARTING: {
+                mNotificationManager.cancel(NOTIFICATION_ID);
                 mNotificationBuilder.mActions.clear();
                 mNotificationBuilder.setProgress(0, 0, true);
                 String text = getString(R.string.download_starting_notification);