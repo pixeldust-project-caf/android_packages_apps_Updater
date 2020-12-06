PixelExperience/packages_apps_Updates

Updates: Add privapp permissions xml
Change-Id: If1953e76859850359a63dc4a4ab50940d76fd59f

Android.mk
@@ -27,7 +27,17 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.pixelexperience.ota.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_org.pixelexperience.ota.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 

pixeldust-project-caf/android_packages_apps_Updater

Signed-off-by: spezi77 <spezi7713@gmx.net>

Android.mk
@@ -27,7 +27,17 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.pixeldust.ota.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.pixeldust.ota.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 